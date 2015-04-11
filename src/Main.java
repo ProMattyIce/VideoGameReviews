f164335e41b28e40423e93c853dd0835672aaae7
Matthew Hoare <pro.mathaore013@gmail.com>
2015-04-11 09:23:32 -0400

    Jar files added

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
-
 public class Main
 {
     public static void main(String args[])
@@ -9,19 +5,7 @@ public class Main
 //        MetaMindResults sample = new MetaMindResults("Matthew Hoare");
 //        System.out.println(sample.toString());
 
-        Game halo = new Game("");
-
-        try {
-            Document doc = Jsoup.connect("http://www.ign.com/articles/2007/09/25/halo-3-collectors-edition-review?page=5").get();
-            Elements newsHeadlines = doc.select(".scorebox_breakdownBoxComment");
-
-            System.out.println(newsHeadlines.text().toString());
-        }
-        catch (Exception e)
-        {
-
-        }
+        //Game halo = new Game("Heros of the Storm");
 
     }
-
 }
