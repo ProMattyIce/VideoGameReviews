64e216a51b3c8c3e653f69cd1c85261e4e58420d
Matthew Hoare <pro.mathaore013@gmail.com>
2015-04-11 07:36:30 -0400

    Added Game Class

---
+++ src/Game.java
@@ -0,0 +1,52 @@
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Iterator;
+
+
+public class Game {
+    private String title;
+    private String platforms;
+
+    public Game(String title) {
+        this.title = title;
+
+        getData(title);
+    }
+
+    private void getData(String title)
+    {
+
+        String temptitle = title.replaceAll(" ", "+");
+
+        try {
+            HttpResponse<JsonNode> response = Unirest.get("https://videogamesrating.p.mashape.com/get.php?count=5&game=" + temptitle)
+                    .header("X-Mashape-Key", "xAhp9BsXBMmshNQtWbda8tCgn0QYp1B0Ef0jsn1F39m8wfoUMh")
+                    .header("Accept", "application/json")
+                    .asJson();
+
+
+            JSONArray jsonarray = new JSONArray(response.getBody().toString());
+
+            for (int i = 0; i < jsonarray.length(); i++)
+            {
+                JSONObject e = jsonarray.getJSONObject(i);
+
+                if(e.getString("title").toUpperCase().replaceAll(" ", "").equals(title.toUpperCase().replaceAll(" ",""))) {
+                    System.out.println(e.get("platforms").toString().substring(e.get("platforms").toString().indexOf("{\"1\":\"") + "{\"1\":\"".length(), e.get("platforms").toString().length() - 2).replace(",", "").replace(":", "").replace("\"", " ").replace(" 3 ", "").replace("2", "").replace("4", "").replace("5", "").trim().replaceAll("\\s+", " "));
+                    break;
+                }
+                ;
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+        }
+    }
+}
+
+
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ public class Main
 {
     public static void main(String args[])
     {
-        MetaMindResults sample = new MetaMindResults("Matthew Hoare");
-        System.out.println(sample.toString());
+//        MetaMindResults sample = new MetaMindResults("Matthew Hoare");
+//        System.out.println(sample.toString());
+
+        Game halo = new Game("Gears of War 3");
+
     }
 
 }
